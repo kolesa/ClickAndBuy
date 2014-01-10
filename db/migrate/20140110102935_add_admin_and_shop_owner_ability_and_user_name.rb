8ef8d3636c117ab5664fe1e869b35d6f6df1bd6a
Sapotero Dominiko <egokok@gmail.com>
2014-01-11 03:18:36 +0400

    heroku fix

--- db/migrate/20140110102935_add_admin_and_shop_owner_ability_and_user_name.rb
+++ db/migrate/20140110102935_add_admin_and_shop_owner_ability_and_user_name.rb
@@ -1,5 +1,9 @@
 class AddAdminAndShopOwnerAbilityAndUserName < ActiveRecord::Migration
   def change
-    rename_column(:users, :firs_name, :first_name)
+    add_column :users, :is_admin, :boolean, :default => false
+    add_column :users, :owned_shop, :integer, :default => 0
+    add_column :users, :firs_name, :string
+    add_column :users, :last_name, :string
+    add_attachment :users, :avatar
   end
 end
