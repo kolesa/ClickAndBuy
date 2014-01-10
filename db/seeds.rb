42f50f498cfcc68e0bff99024a3762d6ac22cbd6
Sapotero Dominiko <egokok@gmail.com>
2014-01-10 14:48:43 +0400

    Role system & Updated user info (First and Last names, Avatar)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = User.create(email: 'admin@click.ru', password: 'qweasdzxc', password_confirmation: 'qweasdzxc', is_admin: 1 )
