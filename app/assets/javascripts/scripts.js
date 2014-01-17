14f660a51eb6764d8de9d4f3f3d0e5e368331ebc
Sapotero Dominiko <egokok@gmail.com>
2014-01-18 01:01:44 +0400

    New Theme | Downgrade to Bootstrap 2.3.2 | Template converting from 3.x to 2.3.x

---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){	
+
+/***************************************************
+	MENU
+***************************************************/
+	jQuery('ul.nav li.dropdown').hover(function (){
+        jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
+    }, function (){
+        jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
+    });
+
+/***************************************************
+		TOOLTIP & POPOVER
+***************************************************/
+$("[rel=tooltip]").tooltip();
+$("[data-rel=tooltip]").tooltip();
+
+/***************************************************
+		CAROUSEL - STOP AUTO CYCLE
+***************************************************/
+ $('.carousel').carousel({
+    interval: false});
+
+/***************************************************
+		HOVERS
+***************************************************/
+	$(".hover_img, .hover_colour").on('mouseover',function(){
+			var info=$(this).find("img");
+			info.stop().animate({opacity:0.1},500);
+		}
+	);
+	$(".hover_img, .hover_colour").on('mouseout',function(){
+			var info=$(this).find("img");
+			info.stop().animate({opacity:1},800);
+		}
+	);
+	
+/***************************************************
+		BACK TO TOP LINK
+***************************************************/
+			$(window).scroll(function() {
+				if ($(this).scrollTop() > 200) {
+					$('.go-top').fadeIn(200);
+				} else {
+					$('.go-top').fadeOut(200);
+				}
+			});
+			
+			// Animate the scroll to top
+			$('.go-top').click(function(event) {
+				event.preventDefault();
+				
+				$('html, body').animate({scrollTop: 0}, 300);
+			})
+		});	
+
+/***************************************************
+	ANIMATIONS
+***************************************************/
+
+$(function() { 	
+$('.welcome').show().addClass("animated fadeInDown");
+$('.welcome_index').show().addClass("animated fadeInDown");
+$('.intro_sections h6').show().addClass("animated fadeInUp");
+$('.fadeinup').show().addClass("animated fadeInUp");
+$('.fadeindown').show().addClass("animated fadeInDown");
+}); 
+
+/***************************************************
+		PRETTYPHOTO
+***************************************************/
+$('a[data-rel]').each(function() {
+$(this).attr('rel', $(this).attr('data-rel')).removeAttr('data-rel');
+});
+$("a[rel^='prettyPhoto']").prettyPhoto();
+	jQuery("a[rel^='prettyPhoto'], a[rel^='lightbox']").prettyPhoto({
+overlay_gallery: false, social_tools: false,  deeplinking: false
+});
+
+
