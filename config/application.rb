296427a12e97fa8c605abe44c82d29cbaf873741
Sapotero Dominiko <egokok@gmail.com>
2014-01-18 20:58:50 +0400

    heroku assets precompile fix

--- config/application.rb
+++ config/application.rb
@@ -12,29 +12,9 @@ module ClickAndBuy
     ActsAsTaggableOn.remove_unused_tags = true
     ActsAsTaggableOn.force_lowercase = true
 
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif,
-                                  "fontawesome-webfont.ttf",
-                                  "fontawesome-webfont.eot",
-                                  "fontawesome-webfont.svg",
-                                  "fontawesome-webfont.woff")
-
-
-
-    config.assets.precompile << Proc.new do |path|
-      if path =~ /\.(css|js)\z/
-        full_path = Rails.application.assets.resolve(path).to_path
-        app_assets_path = Rails.root.join('app', 'assets').to_path
-        if full_path.starts_with? app_assets_path
-          puts "including asset: " + full_path
-          true
-        else
-          puts "excluding asset: " + full_path
-          false
-        end
-      else
-        false
-      end
-    end
+    config.serve_static_assets = true
+    config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+    config.assets.compile = true
     
     # config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
