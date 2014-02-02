c8afc7209a93d302c22a758baef70c6fda78aefc
Sapotero Dominiko <egokok@gmail.com>
2014-02-02 17:17:59 +0400

    Social integration complete | Google fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -36,14 +36,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def odnoklassniki
-    @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
+    @user = User.find_for_odnoklassniki_oauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "odnoklassniki"
       sign_in_and_redirect @user, :event => :authentication
+      set_flash_message(:notice, :success, :kind => "Odnoklassniki") if is_navigational_format?
     else
-      session["devise.google_data"] = request.env["omniauth.auth"]
+      session["devise.odnoklassniki_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
-  end
 end
