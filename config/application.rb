e5ae520f3100743d8bf7d455c37b4d48e836f71d
Sapotero Dominiko <egokok@gmail.com>
2014-02-10 00:57:47 +0400

    OoO

--- config/application.rb
+++ config/application.rb
@@ -17,21 +17,7 @@ module ClickAndBuy
     config.assets.enabled = true
     config.assets.digest = true
 
-    config.assets.precompile << Proc.new do |path|
-      if path =~ /\.(css|js)\z/
-        full_path = Rails.application.assets.resolve(path).to_path
-        app_assets_path = Rails.root.join('app', 'assets').to_path
-        if full_path.starts_with? app_assets_path
-          puts "including asset: " + full_path
-          true
-        else
-          puts "excluding asset: " + full_path
-          false
-        end
-      else
-        false
-      end
-    end
+    config.assets.precompile += ['jquery.js', 'jquery_ujs.js', 'jquery.easypiechart.js']
 
     config.paperclip_defaults = {
         :storage => :s3,
