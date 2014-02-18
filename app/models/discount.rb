ed4d6870279986bedc0e14a5e17419f8eb2b7d10
Sapotero Dominiko <egokok@gmail.com>
2014-02-18 15:40:46 +0400

    discount validation fix

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -15,7 +15,7 @@
 class Discount < ActiveRecord::Base
   has_many :items
 
-  validates :discount, :numericality => { :greater_than => 0 }, allow_blank: true
-  validates :likes,    :numericality => { :greater_than => 0 }, allow_blank: true
-  validates :count,    :numericality => { :greater_than => 0 }, allow_blank: true
+  validates :discount, :numericality => { :greater_than => 0 }, allow_nill: true
+  validates :likes,    :numericality => { :greater_than => 0 }, allow_nill: true
+  validates :count,    :numericality => { :greater_than => 0 }, allow_nill: true
 end
