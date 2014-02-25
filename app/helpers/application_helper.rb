b5b429b31c36c9a00f0d1fa7ce2ab660b5bcc15b
Sapotero Dominiko <egokok@gmail.com>
2014-02-26 00:42:43 +0400

    title fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
   include ActsAsTaggableOn::TagsHelper
+  
+  def full_title(page_title)
+    base_title = "LikeAndBuy"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 
   #def generate_menu
   #  ActsAsTaggableOn::Tag.where(:parent => 0).order(:parent, :order)
