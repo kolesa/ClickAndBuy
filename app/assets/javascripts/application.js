ab5a81470ec1870dbd38ddab331ee4a976975d9e
Sapotero Dominiko <egokok@gmail.com>
2014-02-26 01:51:15 +0400

    3steps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 // require turbolinks
 //= require jquery
 //= require jquery_ujs
-//= require jquery.sortable
 //= require bootstrap.min
 //= require select2
 //= require dataTables/jquery.dataTables
